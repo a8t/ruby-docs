317320b6c730d77d3491c37006861eb4d2462a16
Andy Tran <andy8tran@gmail.com>
2017-08-11 16:20:29 -0400

    done

---
+++ docs.rb
@@ -0,0 +1,35 @@
+## String
+
+# length: finds length of string or array
+
+# strip: removes any whitespace before or after string
+
+# split(arg): finds arg, splits string or array and creates new strings or arrays of the values 'in between' the arg. i.e. arg is the boundary point.
+
+# start_with?(arg): returns boolean of whether the string starts with the arg string
+
+
+
+## Array
+# first(n): returns first n elements of array. without n, returns first element.
+# delete_at(n): deletes object at index n
+# delete(arg): if object in array == arg, remove from array
+# pop(n): removes last n (default 1) elements from array and returns them
+
+
+
+## Hash
+# to_a: converts the hash to an array of [key, value] arrays
+# has_key?: true if given key is in the hash
+# has_value? true if given value is anywhere in the hash
+
+
+
+## Time
+# now: creates time object for current time
+
+
+
+## File
+# exist?(arg): true if named file exists.
+# extname: returns file extension.
